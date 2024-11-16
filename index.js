8c27d4cd5eb926d89188239fef1422ff91c94311
IGNACIO <segoviaignacio98@hotmail.com>
2024-11-16 20:38:24 -0300

    resolucion manipulando el dom

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEl = document.querySelector(".lista");
+  const itemsEls = document.querySelectorAll(".lista li");
+
+  for (let el of itemsEls) {
+    el.remove();
+  }
+
+  for (let item of cosasQueAprendimos) {
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = item.tema;
+    const laClaseQueLeVoyAAgirnarAlLi = item.class || "item";
+    newLiEl.classList.add(laClaseQueLeVoyAAgirnarAlLi);
+    listaEl.appendChild(newLiEl);
+  }
+}
 
 main();
